9e361d26be68bb31f002f67fc3c25bd4d4386059
Mathbech <legeek.you@gmail.com>
2024-10-04 11:24:09 +0200

    Add CoursesCrudController, MarkersCrudController, and TeacherDashboardController

---
+++ src/Controller/Teacher/TeacherDashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Teacher;
+
+use App\Entity\Courses;
+use App\Entity\Markers;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherDashboardController extends AbstractDashboardController
+{
+    #[Route('/dashboard', name: 'teacher_dashboard')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(MarkersCrudController::class)->generateUrl());
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('App');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Markers', 'fas fa-list', Markers::class);
+        yield MenuItem::linkToCrud('Courses', 'fas fa-list', Courses::class);
+    }
+}
