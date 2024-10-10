080b0f5b7ef7e896f081a5fc23951a0c7b9bdf63
Mathbech <legeek.you@gmail.com>
2024-10-10 11:13:38 +0200

    Update Courses entity for API authorization

--- src/Entity/Courses.php
+++ src/Entity/Courses.php
@@ -3,6 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\CoursesRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
@@ -10,7 +14,12 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 #[ORM\Entity(repositoryClass: CoursesRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['pinCode'])]
 #[UniqueEntity(fields: ['pinCode'], message: 'This code is already used')]
-#[ApiResource]
+#[ApiResource(security: "is_granted('ROLE_USER')")]
+#[GetCollection(security: "is_granted('ROLE_ADMIN')")]
+#[Get]
+#[Put]
+#[Post]
+
 class Courses
 {
 
