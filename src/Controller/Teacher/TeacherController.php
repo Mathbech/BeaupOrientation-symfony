88ef1a04820359e88fd51d1401837923c17aed25
Mathbech <legeek.you@gmail.com>
2025-03-13 17:35:44 +0100

    fix: update user retrieval in TeacherController and comment out unused form group in courseAdd template

--- src/Controller/Teacher/TeacherController.php
+++ src/Controller/Teacher/TeacherController.php
@@ -20,7 +20,7 @@ class TeacherController extends AbstractController
     private $user;
     public function __construct(Security $security)
     {
-        $this->user = $security->getUser()->getId();
+        $this->user = $security->getUser();
     }
     #[Route('/', name: 'teacher_home')]
     public function index(EntityManagerInterface $em): Response
