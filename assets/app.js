05725a31773f687ef41425c852cb733fed9f94e7
Mathbech <legeek.you@gmail.com>
2025-07-03 22:00:38 +0200

    Add DataTables Bootstrap 5 integration for improved styling and functionality

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,9 @@
 // import './bootstrap.js';
 import $ from 'jquery';
 import 'datatables.net';
+import 'datatables.net-bs5';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+import 'datatables.net-bs5/js/dataTables.bootstrap5.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.min.js';
 /*
